6506ec410d72bcac84354cb4df2beea621d25f5c
Luyanda <luyandakwazi16@gmail.com>
2025-04-14 17:07:45 +0200

    Its Alive!! It works hehe

--- gameLogic.js
+++ gameLogic.js
@@ -4,13 +4,14 @@ var shuffledLetters = letters.sort(() => Math.random() - 0.5);
 for (let i = 0; i < shuffledLetters.length; i++) {
     const letterBox = document.createElement("div");
     letterBox.innerHTML = shuffledLetters[i];
-    letterBox.className = "letters";
+    letterBox.className = "letter";
 
     letterBox.onclick = function(){
         this.classList.add('lettersFaceUp')
         setTimeout(function(){
             if(document.querySelectorAll('.lettersFaceUp').length > 1){
-                if(document.querySelectorAll('.lettersFaceUp')[0].innerHTML === document.querySelectorAll('.lettersFaceUp')[1].innerHTML){
+                if(document.querySelectorAll('.lettersFaceUp')[0].innerHTML ===
+                 document.querySelectorAll('.lettersFaceUp')[1].innerHTML){
                     document.querySelectorAll('.lettersFaceUp')[0].classList.add('lettersMatch')
                     document.querySelectorAll('.lettersFaceUp')[1].classList.add('lettersMatch')
 
@@ -20,15 +21,14 @@ for (let i = 0; i < shuffledLetters.length; i++) {
                     if(document.querySelectorAll('.lettersMatch').length === letters.length){
                         alert("Congratulation! You Have Won The Game!")
                     }
-
-                    else{
-                        document.querySelectorAll('.lettersFaceUp')[1].classList.remove('lettersFaceUp')
-                        document.querySelectorAll('.lettersFaceUp')[0].classList.remove('lettersFaceUp')
-                    }
+                }
+                else{
+                    document.querySelectorAll('.lettersFaceUp')[1].classList.remove('lettersFaceUp')
+                    document.querySelectorAll('.lettersFaceUp')[0].classList.remove('lettersFaceUp')
                 }
             }
 
-        }, 500)
+        }, 1000)
 
     }
 
